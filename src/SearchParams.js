5fdac739b274b5d9c9b4754ced867aeb57dd742a
Aral Tasher <araltasher@gmail.com>
2020-06-15 23:01:24 -0400

    minor fixes

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import { connect } from "react-redux";
 import useDropdown from "./useDropdown";
